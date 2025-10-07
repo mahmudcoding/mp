9240ffc2986241548a67aa0b747d642211ac035f
Mahmud <mahmudnosirov@icloud.com>
2025-10-07 18:53:26 +0500

    add new features

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,17 @@ import 'pages/page1.dart';
 import 'pages/page2.dart';
 import 'pages/page3.dart';
 
-
 void main() {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Simple E-Commerce',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: MainPage(),
@@ -20,39 +21,125 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class MainPage extends StatelessWidget {
+  final List<Map<String, dynamic>> products = [
+    {
+      "name": "Sneakers",
+      "price": 59.99,
+      "image":
+      "https://cdn-icons-png.flaticon.com/512/148/148812.png"
+    },
+    {
+      "name": "Backpack",
+      "price": 39.50,
+      "image":
+      "https://cdn-icons-png.flaticon.com/512/107/107831.png"
+    },
+    {
+      "name": "Headphones",
+      "price": 79.99,
+      "image":
+      "https://cdn-icons-png.flaticon.com/512/3107/3107988.png"
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('E-Commerce App')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Page1()),
+      appBar: AppBar(
+        title: Text('My Shop'),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Header Text
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Popular Products',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
               ),
-              child: Text('Go to Page 1'),
             ),
-            ElevatedButton(
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Page2()),
-              ),
-              child: Text('Go to Page 2'),
+          ),
+
+          // Product Cards
+          Expanded(
+            child: ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                final product = products[index];
+                return Card(
+                  margin:
+                  const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                  elevation: 3,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: ListTile(
+                    leading: Image.network(
+                      product["image"],
+                      width: 50,
+                      height: 50,
+                    ),
+                    title: Text(product["name"]),
+                    subtitle: Text("\$${product["price"]}"),
+                    trailing: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blueAccent,
+                      ),
+                      child: Text("Buy Now"),
+                    ),
+                  ),
+                );
+              },
             ),
-            ElevatedButton(
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Page3()),
-              ),
-              child: Text('Go to Page 3'),
+          ),
+
+          // Navigation Buttons
+          Container(
+            color: Colors.grey[200],
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton.icon(
+                  icon: Icon(Icons.home),
+                  label: Text("Home"),
+                  onPressed: () {},
+                ),
+                ElevatedButton.icon(
+                  icon: Icon(Icons.category),
+                  label: Text("Categories"),
+                  onPressed: () {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (_) => Page1()));
+                  },
+                ),
+                ElevatedButton.icon(
+                  icon: Icon(Icons.shopping_cart),
+                  label: Text("Cart"),
+                  onPressed: () {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (_) => Page2()));
+                  },
+                ),
+                ElevatedButton.icon(
+                  icon: Icon(Icons.person),
+                  label: Text("Profile"),
+                  onPressed: () {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (_) => Page3()));
+                  },
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
