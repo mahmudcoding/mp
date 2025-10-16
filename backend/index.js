430168bd0d14a72077125160d2a997fad1f3bd4c
Bekmurod O'ktamov <bekmurod2510@gmail.com>
2025-10-16 06:15:17 +0300

    Backend set up
    created and got backend running on localhost:/3000. To test : npm run dev.

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import cors from 'cors'
+
+//Dotenv config - enables taking keys from .env file
+dotenv.config()
+
+//Entry point to backend
+const app = express()
+
+//middlewares
+app.use(morgan())
+app.use(cors())
+app.use(express.json())
+
+//demo route
+app.get('/' , (req, res) => {
+    res.status(200).json({
+        'message' : "hello world!" 
+    })
+})
+
+//app starts
+app.listen(3000 , ()=>{
+    console.log(`listening to ${3000}`)
+})
