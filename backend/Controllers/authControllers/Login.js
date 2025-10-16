d6a0bdebb6c4c054fddac8059f6632c9220403f6
Bekmurod O'ktamov <bekmurod2510@gmail.com>
2025-10-16 20:12:22 +0300

    made Auth functional
    wrote logic for signup and login endpoints, return token and user data.

--- backend/Controllers/authControllers/Login.js
+++ backend/Controllers/authControllers/Login.js
@@ -1,6 +1,54 @@
-export default function login(req, res) {
-    const data = req.body;
-    res.status(200).json({
-        "data" : data,
+//database
+import User from '../../models/User.js'
+import {User as UserAssoc} from '../../models/associations.js'
+
+//Json web token - Auth
+import jwt from 'jsonwebtoken'
+
+
+export default async function login(req, res) {
+    const {email, password} = req.body;
+
+    if (!email || !password) {
+    return res.status(400).json({
+      error: "Email and password are required"
+    });
+  }
+
+   try{
+
+    const existingUser = await UserAssoc.findOne({
+        where : {
+            email : email,
+            password : password
+        }
     })
+
+    if(!existingUser){
+        return res.status(400).json({
+            error : "invalid credentials"
+        })
+    }
+
+
+    const token = jwt.sign(
+        {userID : existingUser.id , email : existingUser.email},
+        process.env.JWT_SECRET,
+        {expiresIn: "7d"}
+    )
+
+    res.status(200).json({
+        messafe : "logged in successfully",
+        token  : token,
+        user : {
+            name : existingUser.name,
+            email : existingUser.email
+        }
+    })
+
+   }catch(err){
+    res.status(400).json({
+        message : err
+    })
+   }
 }
--- backend/Controllers/authControllers/SignUp.js
+++ backend/Controllers/authControllers/SignUp.js
@@ -1,14 +1,42 @@
+//database operations
 import User from '../../models/User.js'
 import {User as UserAssoc} from '../../models/associations.js'
 
+//Json web token
+import jwt from 'jsonwebtoken'
+
+//to get env var
+import dotenv from 'dotenv'
+dotenv.config()
+
 
 export default async function signup(req, res) {
-    try{
     const UserData = req.body
 
-    const addUser = await User.create(UserData)
+    try{
 
-    res.status(200).json(addUser)
+    const isExists = await  UserAssoc.findOne({
+        where : {
+            email : UserData.email
+        }
+    })
+
+    if(isExists){
+        return res.status(401).json("User already exists")
+    }
+
+    const newUser = await User.create(UserData)
+
+    const token = jwt.sign({userID : newUser.id , email : newUser.email}, process.env.JWT_SECRET, {expiresIn : "7d"})
+
+    res.status(200).json({
+        message : "User created successfully",
+        token : token,
+        user : {
+            name : newUser.name,
+            email : newUser.email,
+        }
+    })
 
 
     }catch(err){
